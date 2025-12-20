593218508ae37b05ce97539311683c31d15a3301
Generate1 <94728216+Generate1@users.noreply.github.com>
2025-12-20 19:18:11 +0800

    Add files via upload

---
+++ build/src/io/CMakeFiles/cminus_io.dir/cmake_clean.cmake
@@ -0,0 +1,11 @@
+file(REMOVE_RECURSE
+  "../../libcminus_io.a"
+  "../../libcminus_io.pdb"
+  "CMakeFiles/cminus_io.dir/io.c.o"
+  "CMakeFiles/cminus_io.dir/io.c.o.d"
+)
+
+# Per-language clean rules from dependency scanning.
+foreach(lang C)
+  include(CMakeFiles/cminus_io.dir/cmake_clean_${lang}.cmake OPTIONAL)
+endforeach()
---
+++ build/src/lightir/CMakeFiles/IR_lib.dir/DependInfo.cmake
@@ -0,0 +1,32 @@
+
+# Consider dependencies only in project.
+set(CMAKE_DEPENDS_IN_PROJECT_ONLY OFF)
+
+# The set of languages for which implicit dependencies are needed:
+set(CMAKE_DEPENDS_LANGUAGES
+  )
+
+# The set of dependency files which are needed:
+set(CMAKE_DEPENDS_DEPENDENCY_FILES
+  "/home/skapeach/Desktop/2025ustc-jianmu-compiler/src/lightir/BasicBlock.cpp" "src/lightir/CMakeFiles/IR_lib.dir/BasicBlock.cpp.o" "gcc" "src/lightir/CMakeFiles/IR_lib.dir/BasicBlock.cpp.o.d"
+  "/home/skapeach/Desktop/2025ustc-jianmu-compiler/src/lightir/Constant.cpp" "src/lightir/CMakeFiles/IR_lib.dir/Constant.cpp.o" "gcc" "src/lightir/CMakeFiles/IR_lib.dir/Constant.cpp.o.d"
+  "/home/skapeach/Desktop/2025ustc-jianmu-compiler/src/lightir/Function.cpp" "src/lightir/CMakeFiles/IR_lib.dir/Function.cpp.o" "gcc" "src/lightir/CMakeFiles/IR_lib.dir/Function.cpp.o.d"
+  "/home/skapeach/Desktop/2025ustc-jianmu-compiler/src/lightir/GlobalVariable.cpp" "src/lightir/CMakeFiles/IR_lib.dir/GlobalVariable.cpp.o" "gcc" "src/lightir/CMakeFiles/IR_lib.dir/GlobalVariable.cpp.o.d"
+  "/home/skapeach/Desktop/2025ustc-jianmu-compiler/src/lightir/IRprinter.cpp" "src/lightir/CMakeFiles/IR_lib.dir/IRprinter.cpp.o" "gcc" "src/lightir/CMakeFiles/IR_lib.dir/IRprinter.cpp.o.d"
+  "/home/skapeach/Desktop/2025ustc-jianmu-compiler/src/lightir/Instruction.cpp" "src/lightir/CMakeFiles/IR_lib.dir/Instruction.cpp.o" "gcc" "src/lightir/CMakeFiles/IR_lib.dir/Instruction.cpp.o.d"
+  "/home/skapeach/Desktop/2025ustc-jianmu-compiler/src/lightir/Module.cpp" "src/lightir/CMakeFiles/IR_lib.dir/Module.cpp.o" "gcc" "src/lightir/CMakeFiles/IR_lib.dir/Module.cpp.o.d"
+  "/home/skapeach/Desktop/2025ustc-jianmu-compiler/src/lightir/Type.cpp" "src/lightir/CMakeFiles/IR_lib.dir/Type.cpp.o" "gcc" "src/lightir/CMakeFiles/IR_lib.dir/Type.cpp.o.d"
+  "/home/skapeach/Desktop/2025ustc-jianmu-compiler/src/lightir/User.cpp" "src/lightir/CMakeFiles/IR_lib.dir/User.cpp.o" "gcc" "src/lightir/CMakeFiles/IR_lib.dir/User.cpp.o.d"
+  "/home/skapeach/Desktop/2025ustc-jianmu-compiler/src/lightir/Value.cpp" "src/lightir/CMakeFiles/IR_lib.dir/Value.cpp.o" "gcc" "src/lightir/CMakeFiles/IR_lib.dir/Value.cpp.o.d"
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_LINKED_INFO_FILES
+  )
+
+# Targets to which this target links which contain Fortran sources.
+set(CMAKE_Fortran_TARGET_FORWARD_LINKED_INFO_FILES
+  )
+
+# Fortran module output directory.
+set(CMAKE_Fortran_TARGET_MODULE_DIR "")
